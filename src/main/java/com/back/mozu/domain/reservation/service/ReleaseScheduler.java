5d7c9d37c765f382ac74baea85c8ba56b2a700ef
kyoungtta <rudtj1216@gmail.com>
2026-05-15 17:33:50 +0900

    refactor: reservation Service Java 파일 삭제

--- src/main/java/com/back/mozu/domain/reservation/service/ReleaseScheduler.java
+++
@@ -1,7 +0,0 @@
-package com.back.mozu.domain.reservation.service;
-
-import com.back.mozu.domain.reservation.entity.Reservation;
-
-public interface ReleaseScheduler {
-    void schedule(Reservation reservation);
-}
