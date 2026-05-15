0fe9aa18157e967345c9a784a0454f311502aaf5
5s-mini <dnclsehd123@naver.com>
2026-05-15 16:45:06 +0900

    refactor: 오타 수정

--- src/main/java/com/back/mozu/domain/reservation/controller/ApiV1ReservationController.java
+++ src/main/java/com/back/mozu/domain/reservation/controller/ApiV1ReservationController.java
@@ -6,11 +6,16 @@ import com.back.mozu.domain.reservation.service.ReservationService;
 import com.back.mozu.global.response.Rq;
 import com.back.mozu.global.response.RsData;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/back/mozu/domain/reservation/service/ReservationService.java
+++ src/main/java/com/back/mozu/domain/reservation/service/ReservationService.java
@@ -1,6 +1,5 @@
 package com.back.mozu.domain.reservation.service;
 
-import com.back.mozu.domain.reservation.dto.ReservationDto;
 import com.back.mozu.domain.reservation.entity.Reservation;
 import com.back.mozu.domain.reservation.entity.ReservationStatus;
 import com.back.mozu.domain.reservation.entity.TimeSlot;
