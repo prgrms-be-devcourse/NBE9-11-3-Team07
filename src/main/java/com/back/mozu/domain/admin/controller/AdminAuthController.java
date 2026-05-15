0cf3fffa1a23adf8602f192439083ead5dce57c3
5s-mini <dnclsehd123@naver.com>
2026-05-15 17:08:05 +0900

    refactor: 오타 수정

--- src/main/java/com/back/mozu/domain/admin/controller/AdminAuthController.java
+++ src/main/java/com/back/mozu/domain/admin/controller/AdminAuthController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.Cookie;
 
-
 @RestController
 @RequestMapping("/api/v1/admin/auth")
 @RequiredArgsConstructor
